6eb46b62b642a22c91798acd22a14d81ec7e657b
Collins Ogera <ogeracollins@gmail.com>
2023-09-20 12:01:28 +0300

    Import review

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.review import Review
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
