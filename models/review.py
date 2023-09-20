640b7341f2b6149b8f2db19c2f567c59a685076a
Collins Ogera <ogeracollins@gmail.com>
2023-09-20 11:56:01 +0300

    Added reviews table

--- models/review.py
+++ models/review.py
@@ -14,6 +14,7 @@ class Review(BaseModel, Base):
         place_id (str): Id of place being reviewed
         user_id (str): Id of user who worte the review
     """
+    __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
