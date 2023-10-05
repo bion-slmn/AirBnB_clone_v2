bca0498713cd9bfd2673a5d942e81f6c47391eee
bion-slmn <bionsol25@gmail.com>
2023-10-05 01:14:16 -0700

    tried

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,30 +12,20 @@ env.hosts = ['54.89.179.242', '3.90.83.124']
 def do_deploy(archive_path):
     '''deploys achivef file'''
     try:
-        file_exists = os.path.isfile(archive_path)
-        if not file_exists:
+        if not os.path.exists(archive_path):
             return False
-
-        # removing extension fromthe file name
-        file_name = os.path.basename(archive_path)
-        no_ext = file_name.split('.')[0]
-        dest_dir = f'/data/web_static/releases/{no_ext}'
-
-        # upload to remote
-        put(archive_path, '/tmp/')
-        run(f'sudo rm -rf {dest_dir}')
-        run(f'sudo mkdir -p {dest_dir}')
-
-        # unzip the acrchive
-        run(f'sudo tar -C {dest_dir} -xzf /tmp/{file_name}')
-        # remove the acrchive
-        run(f'sudo rm /tmp/{file_name}')
-        # move the content
-        run(f'sudo mv {dest_dir}/web_static/* {dest_dir}')
-        run(f'sudo rm -rf {dest_dir}/web_static')
-        run(f'sudo rm -rf /data/web_static/current')
-        # create a symblink
-        run(f'sudo ln -s {dest_dir} /data/web_static/current')
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception as e:
